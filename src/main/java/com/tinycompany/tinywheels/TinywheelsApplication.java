4cb646db51d7aac5cdf030fdfe77e88fc13821b9
Bajmo <anas.mouradd@gmail.com>
2023-03-18 13:32:10 +0100

    Initial commit

---
+++ src/main/java/com/tinycompany/tinywheels/TinywheelsApplication.java
@@ -0,0 +1,13 @@
+package com.tinycompany.tinywheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinywheelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinywheelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tinycompany/tinywheels/TinywheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinycompany.tinywheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinywheelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
