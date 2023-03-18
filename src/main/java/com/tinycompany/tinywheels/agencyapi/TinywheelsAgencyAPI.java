4fe2445aea2a5f010cd26c486d98d0b9ec7d6024
Bajmo <anas.mouradd@gmail.com>
2023-03-18 15:16:27 +0100

    Renamed main Java files to clarify the corresponding API role

--- src/main/java/com/tinycompany/tinywheels/agencyapi/TinywheelsAgencyAPI.java
+++ src/main/java/com/tinycompany/tinywheels/agencyapi/TinywheelsAgencyAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TinywheelsApplication {
+public class TinywheelsAgencyAPI {
 
     public static void main(String[] args) {
-        SpringApplication.run(TinywheelsApplication.class, args);
+        SpringApplication.run(TinywheelsAgencyAPI.class, args);
     }
 
 }
