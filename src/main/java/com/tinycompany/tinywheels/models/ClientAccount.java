9b37629d3a98e6430361561eaa43db4ec5625979
Bajmo <anas.mouradd@gmail.com>
2023-03-18 15:06:00 +0100

    Added trajectory and account classes

---
+++ src/main/java/com/tinycompany/tinywheels/models/ClientAccount.java
@@ -0,0 +1,47 @@
+package com.tinycompany.tinywheels.models;
+
+public class ClientAccount {
+    private int id;
+    private String email;
+    private String password;
+    private String type;
+
+    public ClientAccount(int id, String email, String password, String type) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/main/java/com/tinycompany/tinywheels/models/Trajectory.java
@@ -0,0 +1,69 @@
+package com.tinycompany.tinywheels.models;
+
+import java.util.Date;
+
+public class Trajectory {
+    private int id;
+    private int id_start_address;
+    private int id_destination_address;
+    private int id_driver;
+    private Date pickup_date;
+    private int id_client_account;
+
+    public Trajectory(int id, int id_start_address, int id_destination_address, int id_driver, Date pickup_date, int id_client_account) {
+        this.id = id;
+        this.id_start_address = id_start_address;
+        this.id_destination_address = id_destination_address;
+        this.id_driver = id_driver;
+        this.pickup_date = pickup_date;
+        this.id_client_account = id_client_account;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_start_address() {
+        return id_start_address;
+    }
+
+    public void setId_start_address(int id_start_address) {
+        this.id_start_address = id_start_address;
+    }
+
+    public int getId_destination_address() {
+        return id_destination_address;
+    }
+
+    public void setId_destination_address(int id_destination_address) {
+        this.id_destination_address = id_destination_address;
+    }
+
+    public int getId_driver() {
+        return id_driver;
+    }
+
+    public void setId_driver(int id_driver) {
+        this.id_driver = id_driver;
+    }
+
+    public Date getPickup_date() {
+        return pickup_date;
+    }
+
+    public void setPickup_date(Date pickup_date) {
+        this.pickup_date = pickup_date;
+    }
+
+    public int getId_client_account() {
+        return id_client_account;
+    }
+
+    public void setId_client_account(int id_client_account) {
+        this.id_client_account = id_client_account;
+    }
+}
